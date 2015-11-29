1110ae1a12306c00ffdff77946ac215b6aa5545a
Jimmy Lu <gongchuo.lu@gmail.com>
2015-11-28 20:28:24 -0500

    Fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-uberscript "0.1.0-SNAPSHOT"
+(defproject lein-uberscript "0.1.0"
   :description "Embed uberjar into an executable script"
   :url "https://github.com/Yuhta/lein-uberscript"
   :license {:name "Eclipse Public License"
